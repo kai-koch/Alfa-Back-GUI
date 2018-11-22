ca3abf4b53b5f28be9811c6607b93169101d6bfa
Kai Koch <kai.koch@gmail.com>
2018-11-22 12:40:15 +0100

    * Daten.h erweitert + Update class diagram

--- Alfa-Back-GUI/Daten.h
+++ Alfa-Back-GUI/Daten.h
@@ -1,13 +1,35 @@
 #pragma once
 #include "Zutat.h"
+#include "Rezept.h"
 
 using namespace System;
 using namespace System::Collections;
+
 ref class Daten {
 private:
     Daten();
     Daten(const Daten%);
     static Daten instance;
+    Dictionary<String^, Zutat^>^ zutaten = gcnew Dictionary<String^, Zutat^>();
+    Dictionary<String^, Rezept^>^ rezepte = gcnew Dictionary<String^, Rezept^>();
 public:
     static property Daten^ InstanceOf { Daten^ get() { return %instance; } }
+    
+    Dictionary<String^, Zutat^>^ getAllZutaten();
+    Dictionary<String^, Rezept^>^ getAllRezepte();
+
+    Rezept^ getRezeptByName(String^ daName);
+    Zutat^ getZutatByName(String^ daName);
+
+    Void setRezeptByName(Rezept^ daRezept);
+    Void setZutatByName(Zutat^ daZutat);
+
+    Void readFromDataFile();
+    Void readFromDataFile(String^ Filename);
+
+    Void writeToDataFile();
+    Void writeToDataFile(String^ Filename);
+
+    Void writeKonfigDatei(Rezept^ daRezept);
+    Void writeKonfigDatei(Rezept^ daRezept, String^ Filename);
 };
